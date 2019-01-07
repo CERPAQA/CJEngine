49c0b68232d56047d416efa929760dfb77ad43c2
Daryl Stevens <dlstevens91@outlook.com>
2019-01-07 15:22:31 +0000

    Took out judges link

--- CJEngine/ClientApp/src/components/NavMenu.js
+++ CJEngine/ClientApp/src/components/NavMenu.js
@@ -39,9 +39,6 @@ export class NavMenu extends Component {
                 <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
                 </NavItem>
-                <NavItem>
-                    <NavLink tag={Link} className="text-dark" to="/judges">Judges</NavLink>
-                </NavItem>
               </ul>
             </Collapse>
           </Container>
