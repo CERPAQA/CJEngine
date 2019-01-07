354e8229b69b907f2d8365e30c82dd818ee3dcaf
Daryl Stevens <dlstevens91@outlook.com>
2019-01-07 18:10:18 +0000

    Changes to packages including npm version of PDF.js. Removed Pages folder. Added guidence for CJ area of client app.

--- CJEngine/ClientApp/src/components/NavMenu.js
+++ CJEngine/ClientApp/src/components/NavMenu.js
@@ -39,6 +39,9 @@ export class NavMenu extends Component {
                 <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
                 </NavItem>
+                <NavItem>
+                    <NavLink tag={Link} className="text-dark" to="/CJ">CJ</NavLink>
+                </NavItem>
               </ul>
             </Collapse>
           </Container>
