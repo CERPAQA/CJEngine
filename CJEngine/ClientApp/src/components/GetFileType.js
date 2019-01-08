387dbaead0f3a597bdc430abd14f4b3e136cb19f
femi1992 <olufemidokun@hotmail.co.uk>
2019-01-08 15:59:22 +0000

    Testing class component theory on GetFileType

--- CJEngine/ClientApp/src/components/GetFileType.js
+++ CJEngine/ClientApp/src/components/GetFileType.js
@@ -1,4 +1,8 @@
-﻿function GetFileType(filename) {
+﻿import React, { Component } from 'react';
+export class GetFileType extends Component {
+    
+}
+function GetFileType(filename) {
     if (filename != undefined) {
         var x = filename.includes("pdf");
     }
