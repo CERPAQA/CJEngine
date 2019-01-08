362863c208f7d99987b9b7b8fa93fc24a07f0d42
femi1992 <olufemidokun@hotmail.co.uk>
2019-01-08 11:08:07 +0000

    Adding new Components

---
+++ CJEngine/ClientApp/src/components/JudgedScripts.js
@@ -0,0 +1,9 @@
+﻿function TotalScripts(props) {
+    return (
+        <div id="totalScripts">
+            <p>Total pairings: {props.data}</p>
+            <p>Scripts Judged: {props.score}</p>
+            <p>Leading Script: {props.top}</p>
+        </div>
+    );
+}
