67e2169b5f59788d0ecaffb8b2105a1d3f017b29
femi1992 <olufemidokun@hotmail.co.uk>
2019-01-10 09:38:06 +0000

    Working on cj core functionality, sending judgments back to server

--- CJEngine/ClientApp/src/components/IMGViewer.js
+++ CJEngine/ClientApp/src/components/IMGViewer.js
@@ -1,16 +1,8 @@
 ﻿import React, { Component } from 'react';
-
-function importAll(r) {
-    let images = {};
-    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
-    return images;
-}
-
-const images = importAll(require.context('../public', false, '/\.jpg/'));
 export function IMGViewer(props) {
     return (
         <div id="imgOne">
-            <img id={props.id} src={images[props.data]} height='500em' width='500em' ></img>
+            <img id={props.id} src={props.data} height='500em' width='500em' ></img>
         </div>
     );
 }
--- CJEngine/Controllers/PairingsController.cs
+++ CJEngine/Controllers/PairingsController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using CJEngine.Models;
 using System.IO;
 using RDotNet;
+using System.Text;
 
 namespace CJEngine.Controllers
 {
@@ -15,8 +16,12 @@ namespace CJEngine.Controllers
     public class PairingsController : Controller
     {
         private static List<Pairing> allPairings = new List<Pairing>();
+        private static List<Judge> judges = new List<Judge>();
+        private static List<int> ids = new List<int>();
+        private static List<string> scriptsChosen = new List<string>();
+        private static int maxJudges = 5;
 
-		public List<string> GetFiles()
+        public List<string> GetFiles()
         {
             try
             {
@@ -50,7 +55,6 @@ namespace CJEngine.Controllers
         public List<Tuple<int, int>> GetPairings(int noScripts, int noPairings)
         {
             REngineClass.GetREngine().Evaluate(@"source('REngine\\RScripts\\ComparativeJudgmentPairingsTest.R')");
-
            // REngineClass.GetREngine().Evaluate(@"source('C:\\Users\\owner\\Source\\Repos\\CERPAQA\\CJEngine\\CJEngine\\REngine\\RScripts\\ComparativeJudgmentPairingsTest.R')");
             NumericMatrix matrix = REngineClass.GetREngine().Evaluate(string.Format("matrix <- generatePairings(noOfScripts = {0}, noOfPairings = {1})", noScripts, noPairings)).AsNumericMatrix();
 
@@ -78,5 +82,132 @@ namespace CJEngine.Controllers
             }
             return finalResult;
         }
+
+        [HttpGet("[action]")]
+        public int GenerateID()
+        {
+            //not yet working smoothly, needs more testing and work.
+            Random rnd = new Random();
+            int id;
+            do
+            {
+                id = rnd.Next(1, maxJudges);
+            }
+            while (ids.Contains(id));
+            maxJudges++; //Max judges is a low number to keep generated IDs low, it scales up if we want more browser windows to test.
+            ids.Add(id);
+            Judge j = new Judge(id);
+            judges.Add(j);
+            return id;
+        }
+
+        [Produces("application/json")]
+        [Route("winners")]
+        public string GetWinners([FromBody] Pairing data)
+        {
+            string winner = data.winner;
+            Tuple<string, string> pairOfScripts = data.pairOfScripts;
+            string timeJudgement = data.timeJudgement;
+            string elapsedTime = data.elapsedTime;
+            int judgeID = data.judgeID;
+
+            Pairing p = new Pairing(winner, pairOfScripts, timeJudgement, elapsedTime, judgeID);
+            allPairings.Add(p);
+            scriptsChosen.Add(winner);
+            return winner;
+        }
+
+
+        public string GetLeadingScript()
+        {
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+            int compare = 0;
+            string mostFrequent = "";
+            for (var i = 0; i < scriptsChosen.Count; i++)
+            {
+                // this needs changing, otherwise one gets added to each key during each iterations
+                var word = scriptsChosen[i];
+
+                if (counts.ContainsKey(word))
+                {
+                    counts[word] = counts[word] + 1;
+                }
+                else
+                {
+                    counts.Add(word, 1);
+                }
+                if (counts[word] > compare)
+                {
+                    compare = counts[word];
+                    mostFrequent = scriptsChosen[i];
+                }
+                else if (counts[word] == compare)
+                {
+                    mostFrequent = "tie";
+                }
+            }
+            return mostFrequent;
+        }
+
+        public string GenerateCSVString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Winner,");
+            sb.Append("Script One,");
+            sb.Append("Script Two,");
+            sb.Append("Date,");
+            sb.Append("TimeJudged,");
+            sb.Append("ElapsedTime,");
+            sb.Append("Judge");
+            sb.AppendLine();
+            for (int i = 0; i < allPairings.Count; i++)
+            {
+                //Just cleaning the strings up a little bit for easier reading/analysis
+                sb.Append(allPairings[i].winner.Replace("/images/", "") + ",");
+                sb.Append(allPairings[i].pairOfScripts.ToString().Replace("/images/", "").Replace("(", "").Replace(")", "") + ",");
+                sb.Append(allPairings[i].timeJudgement + ",");
+                sb.Append(allPairings[i].elapsedTime + ",");
+                sb.Append(allPairings[i].judgeID + ",");
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+
+        [HttpGet]
+        public FileContentResult ReportBuilder()
+        {
+            var csv = GenerateCSVString();
+            var fileName = "report.csv";
+
+            return File(new System.Text.UTF8Encoding().GetBytes(csv), "text/csv", fileName);
+        }
+
+        public class Pairing
+        {
+            public string winner { get; set; }
+            public Tuple<string, string> pairOfScripts { get; set; }
+            public string timeJudgement { get; set; }
+            public string elapsedTime { get; set; }
+            public int judgeID;
+
+            public Pairing(string winner, Tuple<string, string> pairOfScripts, string timeJudgement, string elapsedTime, int id)
+            {
+                this.winner = winner;
+                this.pairOfScripts = pairOfScripts;
+                this.timeJudgement = timeJudgement;
+                this.elapsedTime = elapsedTime;
+                this.judgeID = id;
+            }
+        }
+
+        public class Judge
+        {
+            public int judgeID { get; set; }
+            public Judge(int judgeID)
+            {
+                this.judgeID = judgeID;
+            }
+        }
     }
 }
+
