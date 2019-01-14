3ee67206752348f6bc5d49999bce734c8ebbfab6
femi1992 <olufemidokun@hotmail.co.uk>
2019-01-14 11:15:23 +0000

    PDFViewer now working, not quite yet sure what folder stucture allowed it to work

--- CJEngine/ClientApp/src/components/PDFViewer.js
+++ CJEngine/ClientApp/src/components/PDFViewer.js
@@ -2,7 +2,7 @@
 export function PDFViewer(props) {
     return (
         <div id="pdfOne">
-            <iframe id={props.id} src={"/ClientApp/public/Root/pdfjs-2.0.943-dist/web/web/viewer.html?file="+ props.data} height='500em' width='500em'> </iframe>
+            <iframe id={props.id} src={props.data} height='500em' width='500em'> </iframe>
         </div>
     );
 }
