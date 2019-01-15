8650cfa85d6c28008e0ad8a07999d9252e8d289a
femi1992 <olufemidokun@hotmail.co.uk>
2019-01-15 16:09:34 +0000

    Artefacts details page working now

---
+++ CJEngine/Migrations/20190115152956_newField.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CJEngine.Migrations
+{
+    public partial class newField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Experiment",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<string>(
+                name: "FilePath",
+                table: "Artefact",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FilePath",
+                table: "Artefact");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Experiment",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
