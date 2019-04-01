5d2774f330dc0dcbbad7a65a64c1cec660e510e5
femi1992 <olufemidokun@hotmail.co.uk>
2019-04-01 15:08:39 +0100

    timed judgements  partially opeational

--- CJEngine/ClientApp/src/components/Clock.js
+++ CJEngine/ClientApp/src/components/Clock.js
@@ -2,31 +2,46 @@
 export class Clock extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { isHidden: false };
+        this.state = { timer: 0 };
     }
 
-    toggleHidden() {
-        this.setState({ isHidden: !this.state.isHidden })
+    componentDidMount() {
+        var stringExpNum = document.URL.split("/")[4];
+        var expNum = parseInt(stringExpNum, 10);
+        fetch("api/Pairings/GetParams/?id=" + expNum)
+            .then(response => response.json())
+            .then(params => {
+                this.setState({ showTimer: params["showTimer"]})
+            });
     }
 
     render() {
         return (
-            <br/>
+            <h2>Elapsed Time: {updateClock()}</h2>
             );
     }
 }
 
-function currentTime() {
+function Duration() {
     return (
-        <div>
-            <h2>{new Date().toLocaleTimeString()}</h2>
-        </div>
+        <h2>Elapsed Time: {updateClock()}</h2>
     );
 }
 
-/*<div >
-    {!this.state.isHidden && <Tick />}
-    <button class="btn btn-dark" onClick={this.toggleHidden.bind(this)} >
-        Hide Time
-				</button>
-</div>*/
+function updateClock() {
+    var currDate = new Date();
+    var diff = currDate - markDate;
+    return (format(diff / 1000));
+
+}
+
+function format(seconds) {
+    var numhours = parseInt(Math.floor(((seconds % 31536000) % 86400) / 3600), 10);
+    var numminutes = parseInt(Math.floor((((seconds % 31536000) % 86400) % 3600) / 60), 10);
+    var numseconds = parseInt((((seconds % 31536000) % 86400) % 3600) % 60, 10);
+    return ((numhours < 10) ? "0" + numhours : numhours)
+        + ":" + ((numminutes < 10) ? "0" + numminutes : numminutes)
+        + ":" + ((numseconds < 10) ? "0" + numseconds : numseconds);
+}
+
+var markDate = new Date();
--- CJEngine/ClientApp/src/index.js
+++ CJEngine/ClientApp/src/index.js
@@ -5,9 +5,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {  } from './components/CJCore';
+import { CJCore } from './components/CJCore';
 //import { ElapsedTimer } from './components/ElapsedTimer';
-//import { Clock } from './components/Clock';
+import { Clock } from './components/Clock';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
@@ -18,12 +18,17 @@ ReactDOM.render(
   </BrowserRouter>,
     rootElement);
 
-/*function Update() {
+function Update() {
     ReactDOM.render(
         <Clock />,
-        document.getElementById('root')
+        document.getElementById('elapsedTime'),
     );
-}*/
-//setInterval(Update, 1000);
-//setInterval(CJCore.judgePair("item1"), 15000); //tried to use class method
+}
+
+function Judge() {
+    document.getElementById("itemOne").click();
+}
+
+setInterval(Update, 1000);
+//setInterval(Judge, 5000); //this works!!!
 registerServiceWorker();
